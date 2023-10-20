6b0d532272ad53009e1892961da40bdfc07147c3
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-10-20 14:30:53 +0200

    feat: install graphql

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { GluestackUIProvider, config } from "@gluestack-ui/themed";
 import Navigator from "@navigator";
 import "react-native-gesture-handler";
 
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+const client = new ApolloClient({
+  uri: apiUrl,
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <GluestackUIProvider config={config}>
-      <Navigator />
-    </GluestackUIProvider>
+    <ApolloProvider client={client}>
+      <GluestackUIProvider config={config}>
+        <Navigator />
+      </GluestackUIProvider>
+    </ApolloProvider>
   );
 }
