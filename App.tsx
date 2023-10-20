ce7c1e291042aefbfb6903f86bb12ad5a9a4369a
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-10-20 14:27:00 +0200

    feat:install ui lib

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
+import { GluestackUIProvider, config } from "@gluestack-ui/themed";
 import Navigator from "@navigator";
 import "react-native-gesture-handler";
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <GluestackUIProvider config={config}>
+      <Navigator />
+    </GluestackUIProvider>
+  );
 }
